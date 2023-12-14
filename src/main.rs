6316341331b2033d0ca85fcc69ad110023dddd7f
Douglas Poma <douglaspoma@yahoo.com>
2023-12-14 10:32:24 -0300

    feat: ensure insert and list tasks

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
 mod model;
 mod schema;
 mod services;
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+
+pub struct AppState {
+    db: Pool<Postgres>,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().configure(services::config))
-        .bind(("127.0.0.1", 4000))?
-        .run()
+    println!("Server running at http://localhost:4000/");
+    dotenv().ok();
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = match PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
         .await
+    {
+        Ok(pool) => {
+            println!("Database connected");
+            pool
+        }
+        Err(error) => {
+            println!("Database connection failed: {:?}", error);
+            std::process::exit(1);
+        }
+    };
+
+    HttpServer::new(move || {
+        App::new()
+            .configure(services::config)
+            .app_data(web::Data::new(AppState { db: pool.clone() }))
+    })
+    .bind(("127.0.0.1", 4000))?
+    .run()
+    .await
 }
--- src/services.rs
+++ src/services.rs
@@ -1,9 +1,15 @@
 use actix_web::{
-    get,
-    web::{scope, ServiceConfig},
+    get, post,
+    web::{scope, Data, Json, Query, ServiceConfig},
     HttpResponse, Responder,
 };
 use serde_json::json;
+
+use crate::{
+    model::TaskModel,
+    schema::{CreateTaskSchema, FilterOptions},
+    AppState,
+};
 
 #[get("/healthChecker")]
 async fn health_checker() -> impl Responder {
@@ -15,6 +21,71 @@ async fn health_checker() -> impl Responder {
 }
 
 pub fn config(conf: &mut ServiceConfig) {
-    let scope = scope("/api").service(health_checker);
+    let scope = scope("/api")
+        .service(health_checker)
+        .service(create_task)
+        .service(list_tasks);
     conf.service(scope);
+}
+
+#[post("/tasks")]
+async fn create_task(body: Json<CreateTaskSchema>, context: Data<AppState>) -> impl Responder {
+    match sqlx::query_as!(
+        TaskModel,
+        "INSERT INTO tasks (title, content) VALUES ($1, $2) RETURNING *",
+        body.title.to_string(),
+        body.content.to_string(),
+    )
+    .fetch_one(&context.db)
+    .await
+    {
+        Ok(task) => {
+            let note_response = json!({
+                "status": "success",
+                "task": task
+            });
+            return HttpResponse::Ok().json(note_response);
+        }
+        Err(error) => {
+            println!("Error creating task: {:?}", error);
+            return HttpResponse::InternalServerError().json(json!({
+                "status": "error",
+                "message": format!("{:?}",error)
+            }));
+        }
+    }
+}
+
+#[get("/tasks")]
+async fn list_tasks(
+    filter_options: Query<FilterOptions>,
+    context: Data<AppState>,
+) -> impl Responder {
+    let limit = filter_options.limit.unwrap_or(10);
+    let offset = (filter_options.page.unwrap_or(1) - 1) * limit;
+    match sqlx::query_as!(
+        TaskModel,
+        "SELECT * FROM tasks ORDER BY id LIMIT $1 OFFSET $2",
+        limit as i32,
+        offset as i32
+    )
+    .fetch_all(&context.db)
+    .await
+    {
+        Ok(tasks) => {
+            let tasks_response = json!({
+                "status": "success",
+                "result": tasks.len(),
+                "tasks": tasks
+            });
+            return HttpResponse::Ok().json(tasks_response);
+        }
+        Err(error) => {
+            println!("Error listing tasks: {:?}", error);
+            return HttpResponse::InternalServerError().json(json!({
+                "status": "error",
+                "message": format!("{:?}",error)
+            }));
+        }
+    }
 }
