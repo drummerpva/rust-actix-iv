b0d836017d7e95307e1434c30dea17195c15e3c9
Douglas Poma <douglaspoma@yahoo.com>
2023-12-15 10:10:06 -0300

    feat: add info rest logger

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod model;
 mod schema;
 mod services;
-use actix_web::{web, App, HttpServer};
+use actix_web::{middleware::Logger, web, App, HttpServer};
 use dotenv::dotenv;
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
@@ -12,7 +12,11 @@ pub struct AppState {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Server running at http://localhost:4000/");
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "actix_web=info");
+    }
     dotenv().ok();
+    env_logger::init();
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let pool = match PgPoolOptions::new()
         .max_connections(10)
@@ -31,8 +35,9 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         App::new()
-            .configure(services::config)
             .app_data(web::Data::new(AppState { db: pool.clone() }))
+            .configure(services::config)
+            .wrap(Logger::default())
     })
     .bind(("127.0.0.1", 4000))?
     .run()
